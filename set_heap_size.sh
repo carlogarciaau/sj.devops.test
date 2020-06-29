0f8612b657bd06c17d6963aa2ff6bb1301c93cf9
Carlo Garcia <garciacarlo@gmail.com>
2020-06-29 17:13:46 +1000

    fix docker build and sed steps; add configmap creation

--- set_heap_size.sh
+++ set_heap_size.sh
@@ -3,7 +3,12 @@
 HEAP_SIZE=$1
 REQUESTS_LIMITS=$2
 
-docker build --build-arg HEAP_SIZE=${HEAP_SIZE} -t devops:test
-sed -i "s/\{\{MEMORY\}\}/${REQUESTS_LIMITS}/g" ../hello-world-deploy.yaml
-kubectl apply -f ../hello-world-deploy.yaml
+docker build --build-arg HEAP_SIZE=${HEAP_SIZE} -t devops:test .
+sed -i.bak "s/{{MEMORY}}/${REQUESTS_LIMITS}/g" hello-world-deploy.yaml
+
+# create configmap
+kubectl create configmap hello-world-config --from-file application.properties
+
+# deployment
+kubectl apply -f hello-world-deploy.yaml
 
